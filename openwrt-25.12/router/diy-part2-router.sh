96a34c52a39d5dd4eca70f2c9e2d5480112d9608
walk6834 <glacier0315@126.com>
2026-07-21 23:02:09 +0800

    ci(workflow): 重构配置生成逻辑，移除非必要的内置配置脚本
    1. 移除工作流中内置的默认配置生成步骤，改用自定义default.config文件
    2. 移除两个DIY脚本中的重复配置工具函数和冗余的包增删配置代码
    3. 统一简化镜像分区、GRUB超时等配置逻辑

--- openwrt-25.12/router/diy-part2-router.sh
+++ openwrt-25.12/router/diy-part2-router.sh
@@ -10,34 +10,6 @@
 # See /LICENSE for more information.
 #
 
-function config_del() {
-	yes="CONFIG_$1=y"
-	no="# CONFIG_$1 is not set"
-
-	sed -i "s/$yes/$no/" .config
-}
-
-function config_add() {
-	yes="CONFIG_$1=y"
-	no="# CONFIG_$1 is not set"
-
-	sed -i "s/${no}/${yes}/" .config
-
-	if ! grep -q "$yes" .config; then
-		echo "$yes" >>.config
-	fi
-}
-
-function config_package_del() {
-	package="PACKAGE_$1"
-	config_del $package
-}
-
-function config_package_add() {
-	package="PACKAGE_$1"
-	config_add $package
-}
-
 function drop_package() {
 	if [ "$1" != "golang" ]; then
 		# feeds/base -> package
@@ -62,74 +34,6 @@ sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generat
 # Modify default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# 删除
-# Firmware
-#config_package_del i915-firmware-dmc
-# Sound Support
-config_package_del kmod-sound-core
-# Video Support
-config_package_del kmod-acpi-video
-config_package_del kmod-backlight
-config_package_del kmod-drm
-config_package_del kmod-drm-buddy
-config_package_del kmod-drm-display-helper
-config_package_del kmod-drm-exec
-config_package_del kmod-drm-i915
-config_package_del kmod-drm-kms-helper
-config_package_del kmod-drm-suballoc-helper
-config_package_del kmod-drm-ttm
-config_package_del kmod-drm-ttm-helper
-config_package_del kmod-fb
-config_package_del kmod-fb-cfb-copyarea
-config_package_del kmod-fb-cfb-fillrect
-config_package_del kmod-fb-cfb-imgblt
-config_package_del kmod-fb-sys-fops
-config_package_del kmod-fb-sys-ram
-# Other
-config_package_del luci-app-rclone_INCLUDE_rclone-webui
-config_package_del luci-app-rclone_INCLUDE_rclone-ng
-
-# 新增
-# Firmware
-config_package_add intel-microcode
-# luci
-config_package_add luci
-config_package_add default-settings-chn
-# bbr
-config_package_add kmod-tcp-bbr
-# coremark cpu 跑分
-config_package_add coremark
-# autocore + lm-sensors-detect： cpu 频率、温度
-config_package_add autocore
-config_package_add lm-sensors-detect
-# nano 替代 vim
-config_package_add nano
-# upnp
-config_package_add luci-app-upnp
-# autoreboot
-#config_package_add luci-app-autoreboot
-# python3
-#config_package_add python3
-#config_package_add python3-base
-#config_package_add python3-pip
-# tty 终端
-config_package_add luci-app-ttyd
-# docker
-config_package_add luci-app-dockerman
-# kms
-config_package_add luci-app-vlmcsd
-# usb 2.0 3.0 支持
-config_package_add kmod-usb2
-config_package_add kmod-usb3
-# usb 网络支持
-config_package_add usbmuxd
-config_package_add usbutils
-config_package_add usb-modeswitch
-config_package_add kmod-usb-serial
-config_package_add kmod-usb-serial-option
-config_package_add kmod-usb-net-rndis
-config_package_add kmod-usb-net-ipheth
-
 # 第三方软件包
 mkdir -p package/custom
 git clone -b openwrt-25.12 --single-branch --depth 1 https://github.com/217heidai/OpenWrt-Packages.git package/custom
@@ -137,50 +41,3 @@ clean_packages package/custom
 ## golang
 rm -rf feeds/packages/lang/golang
 mv package/custom/golang feeds/packages/lang/
-## argon 主题
-config_package_add luci-theme-argon
-## passwall
-config_package_add luci-app-passwall
-config_package_del luci-app-passwall_Iptables_Transparent_Proxy
-config_package_add luci-app-passwall_Nftables_Transparent_Proxy
-config_package_add luci-app-passwall_INCLUDE_Geoview
-config_package_del luci-app-passwall_INCLUDE_Haproxy
-config_package_del luci-app-passwall_INCLUDE_Hysteria
-config_package_del luci-app-passwall_INCLUDE_NaiveProxy
-config_package_del luci-app-passwall_INCLUDE_Shadowsocks_Rust_Client
-config_package_del luci-app-passwall_INCLUDE_Shadowsocks_Rust_Server
-config_package_del luci-app-passwall_INCLUDE_ShadowsocksR_Libev_Client
-config_package_del luci-app-passwall_INCLUDE_ShadowsocksR_Libev_Server
-config_package_del luci-app-passwall_INCLUDE_Shadow_TLS
-config_package_del luci-app-passwall_INCLUDE_Simple_Obfs
-config_package_del luci-app-passwall_INCLUDE_SingBox
-config_package_del luci-app-passwall_INCLUDE_V2ray_Geodata
-config_package_del luci-app-passwall_INCLUDE_V2ray_Plugin
-config_package_add luci-app-passwall_INCLUDE_Xray
-config_package_del luci-app-passwall_INCLUDE_Xray_Plugin
-## 定时任务。重启、关机、重启网络、释放内存、系统清理、网络共享、关闭网络、自动检测断网重连、MWAN3负载均衡检测重连、自定义脚本等10多个功能
-#config_package_add luci-app-taskplan
-#config_package_add luci-lib-ipkg
-#config_package_add bash
-## 分区扩容。一键自动格式化分区、扩容、自动挂载插件，专为OPENWRT设计，简化OPENWRT在分区挂载上烦锁的操作
-config_package_add luci-app-partexp
-## iStore 应用市场
-#config_package_add luci-app-store
-## qmodem 4G/5G 支持
-config_package_add luci-app-qmodem-next
-## luci-app-easytier
-config_package_add luci-app-easytier
-config_package_add easytier
-
-# 镜像生成
-# 修改分区大小
-sed -i "/CONFIG_TARGET_KERNEL_PARTSIZE/d" .config
-echo "CONFIG_TARGET_KERNEL_PARTSIZE=32" >>.config
-sed -i "/CONFIG_TARGET_ROOTFS_PARTSIZE/d" .config
-echo "CONFIG_TARGET_ROOTFS_PARTSIZE=2048" >>.config
-# 调整 GRUB_TIMEOUT
-sed -i "s/CONFIG_GRUB_TIMEOUT=\"3\"/CONFIG_GRUB_TIMEOUT=\"1\"/" .config
-## 不生成 EXT4 硬盘格式镜像
-config_del TARGET_ROOTFS_EXT4FS
-## 不生成非 EFI 镜像
-config_del GRUB_IMAGES
